c12bcc70a724b787821bda9d99e5b83d1b8233fe
Aron <aron.burjan@gmail.com>
2023-06-02 16:36:15 +0200

    Get students integration test

--- src/test/java/com/example/crudmicroservice/CrudMicroserviceApplicationTests.java
+++ src/test/java/com/example/crudmicroservice/CrudMicroserviceApplicationTests.java
@@ -1,12 +1,18 @@
 package com.example.crudmicroservice;
 import com.example.crudmicroservice.student.Student;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -31,11 +37,21 @@ public class CrudMicroserviceApplicationTests {
 	}
 
 	//add student integration test
+
 	@Test
 	public void testAddStudent(){
 		Student student = new Student("Saul Goodman", "saul.goodman@gmail.com");
 		Student response = restTemplate.postForObject(baseUrl, student, Student.class);
-		assertEquals("Saul Goodman", response.getName());
+		assertEquals("Hank Schrader", response.getName());
 		assertEquals(1,testStudentRepository.findAll().size());
 	}
+
+	//get students integration test
+	@Test
+	@Sql(statements = "INSERT INTO Student (Id, name, email) VALUES (UUID(), 'Saul Goodman', 'saul.goodman@gmail.com'), (UUID(), 'Lalo Salamanca', 'lalo.salamanca@gmail.com')",executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+	public void testGetStudents(){
+		List<Student> responseList = restTemplate.getForObject(baseUrl, List.class);
+		assertEquals(2, responseList.size());
+	}
+
 }
