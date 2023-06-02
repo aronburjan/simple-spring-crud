9474786fd43667863da88660b43c789cb91be90c
Aron <aron.burjan@gmail.com>
2023-06-02 13:31:18 +0200

    Simple logging

--- src/main/java/com/example/crudmicroservice/address/AddressController.java
+++ src/main/java/com/example/crudmicroservice/address/AddressController.java
@@ -1,7 +1,10 @@
 package com.example.crudmicroservice.address;
 
+import com.example.crudmicroservice.student.ProfileController;
 import com.example.crudmicroservice.student.ProfileNotFoundException;
 import com.example.crudmicroservice.student.Student;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,8 +19,11 @@ public class AddressController {
         this.addressRepository = addressRepository;
     }
 
+    private final Logger logger = LoggerFactory.getLogger(AddressController.class);
+
     @GetMapping("/addresses/{id}")
     Address getAddressById(@PathVariable UUID id){
+        logger.trace("AddressController GET by Id accessed");
         Address address = addressRepository.findById(id).orElseThrow(() -> new AddressNotFoundException(id));
         return address;
     }
--- src/main/java/com/example/crudmicroservice/student/ProfileController.java
+++ src/main/java/com/example/crudmicroservice/student/ProfileController.java
@@ -1,5 +1,7 @@
 package com.example.crudmicroservice.student;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +14,7 @@ import java.util.regex.Pattern;
 @RestController
 public class ProfileController {
     private final StudentRepository studentRepository;
+    private final Logger logger = LoggerFactory.getLogger(ProfileController.class);
 
     public ProfileController(StudentRepository studentRepository) {
         this.studentRepository = studentRepository;
@@ -19,10 +22,12 @@ public class ProfileController {
 
     @PostMapping("/students")
     Student newStudent(@RequestBody Student newStudent) {
+        logger.trace("ProfileController POST accessed");
         Pattern emailPattern = Pattern.compile("[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}");
         Student student = newStudent;
         Matcher mat = emailPattern.matcher(student.getEmail());
         if(studentRepository.findByEmail(student.getEmail()).size() > 0 || !(mat.matches())){
+            logger.error("Invalid email error");
             throw(new InvalidEmailException(student.getEmail()));
         }else{
             return studentRepository.save(student);
@@ -31,23 +36,27 @@ public class ProfileController {
 
     @GetMapping("/students")
     List<Student> getStudents() {
+        logger.trace("ProfileController GET accessed");
         List<Student> studentList = studentRepository.findAll();
         return studentList;
     }
 
     @GetMapping("/students/{id}")
     Student getStudentById(@PathVariable UUID id){
+        logger.trace("ProfileController GET by id accessed");
         Student student = studentRepository.findById(id).orElseThrow(() -> new ProfileNotFoundException(id));
         return student;
     }
 
     @PutMapping("/students/{id}")
     Student updateStudent(@RequestBody Student newStudent, @PathVariable UUID id){
+        logger.trace("ProfileController PUT accessed");
         Pattern emailPattern = Pattern.compile("[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}");
         Student student = studentRepository.findById(id).orElseThrow(() -> new ProfileNotFoundException(id));
         Student updateStudent = newStudent;
         Matcher mat = emailPattern.matcher(updateStudent.getEmail());
         if(studentRepository.findByEmail(updateStudent.getEmail()).size() > 0 || !(mat.matches())){
+            logger.error("Invalid email error");
             throw(new InvalidEmailException(updateStudent.getEmail()));
         }else {
             student.setName(newStudent.getName());
@@ -58,6 +67,7 @@ public class ProfileController {
 
     @DeleteMapping("/students/{id}")
     void deleteStudent(@PathVariable UUID id){
+        logger.trace("ProfileController DELETE by id accessed");
         studentRepository.deleteById(id);
     }
 }
